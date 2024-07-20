07b5cefc04ebba87cd55ec5ea4f34181bdb40d19
Euller Gabriel <eullergoliveira@gmail.com>
2024-07-20 14:39:33 +0100

    Add imageView image selection, settings button and play button logic
    Started to construct prepare method

--- GameHUB/settingsViewController.swift
+++ GameHUB/settingsViewController.swift
@@ -7,8 +7,23 @@ class settingsViewController: UIViewController {
     @IBOutlet weak var diceNumberImageView: UIImageView!
     @IBOutlet weak var roundNumberImageView: UIImageView!
     @IBOutlet weak var playerNumberSelect: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
     
-     var playerNumber: String = "2"
+     var _playerNumber: Int = 2
+     var _dicesToPlay: Int?
+     var _roundsToPlay: Int?
+    var playerNumber: Int {
+        return _playerNumber
+    }
+//    ForceUnwrap usado pois o safeUnwrap foi garantido na linha 88, liberando o acesso ao botão Jogar. variaveis dicesToPlay e roundsToPlay serão usadas apenas no metodo prepare, que  é condicionado ao uso do botao Jogar.
+    fileprivate var dicesToPlay: Int{
+        return _dicesToPlay!
+    }
+    fileprivate var roundsToPlay: Int{
+        return _roundsToPlay!
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +36,8 @@ class settingsViewController: UIViewController {
         let optionClosure = { (action: UIAction) in
                 print("Pop-up action")
 //            armazena a seleção do usuario numa variavel para que a info seja enviada para a próxima ViewController
-            self.playerNumber = action.title
-            print(self.playerNumber)
+            self._playerNumber = Int(action.title) ?? 0
+            print(self._playerNumber)
             }
 //            Popula o menu com as opções desejadas
             playerNumberSelect.menu = UIMenu(children: [
@@ -35,19 +50,53 @@ class settingsViewController: UIViewController {
             playerNumberSelect.showsMenuAsPrimaryAction = true
     }
     
+//    metodo para selecionar configurações por lançamento de dado
+    func diceSelector() -> Int{
+//        cria objeto a partir da struct DiceGame para acesso ao método throwDice
+        let dice = DiceGame()
+        return dice.throwDice()
+    }
+    
+//    metodo para atualizar a imageView com o dado respectivo ao lançamento
+    func updateDiceImageView(this imageView: UIImageView) -> Int{
+        let throwResult = diceSelector()
+        imageView.image = UIImage(named: "dado" + String(throwResult))
+        
+//        Retorna o resultado do lançamento do dado para popular os parâmetros do jogo,
+//        numero de dados e rounds a serem jogados
+        return throwResult
+    }
+    
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let gameVC = segue.destination as? gameViewController else {return}
+        gameVC.numberOfPlayers = playerNumber
+        gameVC.dicesToPlay = dicesToPlay
+        gameVC.roundsToPlay = roundsToPlay
     }
-    */
+    
    
     
     @IBAction func defineSettingsBtn(_ sender: UIButton) {
+//        Lança os dados para definir os parâmetros de numero de dados e numero de rounds a serem jogados e atualiza a ImageView com os respetivos dados
+        _dicesToPlay =  updateDiceImageView(this: diceNumberImageView)
+        _roundsToPlay =  updateDiceImageView(this: roundNumberImageView)
+        
+//        Torna as ImageViews visíveis
+        diceNumberImageView.isHidden = false
+        roundNumberImageView.isHidden = false
+//      torna o botão Jogar visivel apenas se os parâmetros forem definidos
+        guard let _dicesToPlay else {return}
+        guard let _roundsToPlay else {return}
+        guard _playerNumber != 0 else {return}
+//        guard  case _dicesToPlay ,  case _roundsToPlay, _playerNumber != 0 else {return}
+        playButton.isHidden = false
+        
+        settingsButton.isHidden = true
     }
     
     @IBAction func playBtn(_ sender: UIButton) {
