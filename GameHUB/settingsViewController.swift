62cb4d77654d03a40766eb7f3eee8e7f20ffebd1
Euller Gabriel <eullergoliveira@gmail.com>
2024-07-21 15:39:13 +0100

    REFACTOR: Adapted settingsViewControllet to work using DiceGameManager object functionality
    Removed parameter that were migrated to DiceGameManager class
    Adapted settingsViewController class functions to work with DiceGameManager object parameters

--- GameHUB/settingsViewController.swift
+++ GameHUB/settingsViewController.swift
@@ -11,22 +11,8 @@ class settingsViewController: UIViewController {
     @IBOutlet weak var settingsButton: UIButton!
     
     private var diceGameManager = DiceGameManager()
-    
-     var _playerNumber: Int = 2
-     var _dicesToPlay: Int?
-     var _roundsToPlay: Int?
-    var playerNumber: Int {
-        return _playerNumber
-    }
-//    ForceUnwrap usado pois o safeUnwrap foi garantido na linha 88, liberando o acesso ao botão Jogar. variaveis dicesToPlay e roundsToPlay serão usadas apenas no metodo prepare, que  é condicionado ao uso do botao Jogar.
-    fileprivate var dicesToPlay: Int{
-        return _dicesToPlay!
-    }
-    fileprivate var roundsToPlay: Int{
-        return _roundsToPlay!
-    }
-    
-    
+    private var selectedPlayers: String = ""
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         onPlayerNumberSelect()
@@ -38,8 +24,10 @@ class settingsViewController: UIViewController {
         let optionClosure = { (action: UIAction) in
                 print("Pop-up action")
 //            armazena a seleção do usuario numa variavel para que a info seja enviada para a próxima ViewController
-            self.diceGameManager._playerNumber = Int(action.title) ?? 0
-            print(self.diceGameManager._playerNumber)
+            self.selectedPlayers = action.title
+//            armazena a seleção do usuario no objeto diceGameManager
+            self.diceGameManager._playerNumber = Int(self.selectedPlayers) ?? 0
+        
             }
 //            Popula o menu com as opções desejadas
             playerNumberSelect.menu = UIMenu(children: [
@@ -56,7 +44,7 @@ class settingsViewController: UIViewController {
     
 //    metodo para atualizar a imageView com o dado respectivo ao lançamento
     func updateDiceImageView(this imageView: UIImageView) -> Int{
-        let throwResult = diceGameManager.diceSelector()
+        let throwResult = self.diceGameManager.diceSelector()
         imageView.image = UIImage(named: "dado" + String(throwResult))
         
 //        Retorna o resultado do lançamento do dado para popular os parâmetros do jogo,
@@ -71,29 +59,34 @@ class settingsViewController: UIViewController {
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let gameVC = segue.destination as? gameViewController else {return}
-        gameVC.numberOfPlayers = _playerNumber
-        gameVC.dicesToPlay = _dicesToPlay ?? 0
-        gameVC.roundsToPlay = _roundsToPlay ?? 0
+        gameVC.numberOfPlayers = self.diceGameManager._playerNumber
+        gameVC.dicesToPlay = self.diceGameManager._dicesToPlay ?? 0
+        gameVC.roundsToPlay = self.diceGameManager._roundsToPlay ?? 0
     }
     
    
     
     @IBAction func defineSettingsBtn(_ sender: UIButton) {
 //        Lança os dados para definir os parâmetros de numero de dados e numero de rounds a serem jogados e atualiza a ImageView com os respetivos dados
-        _dicesToPlay =  updateDiceImageView(this: diceNumberImageView)
-        _roundsToPlay =  updateDiceImageView(this: roundNumberImageView)
+        self.diceGameManager._dicesToPlay =  updateDiceImageView(this: diceNumberImageView)
+        self.diceGameManager._roundsToPlay =  updateDiceImageView(this: roundNumberImageView)
         
 //        Torna as ImageViews visíveis
         diceNumberImageView.isHidden = false
         roundNumberImageView.isHidden = false
 //      torna o botão Jogar visivel apenas se os parâmetros forem definidos
-        guard let _dicesToPlay else {return}
-        guard let _roundsToPlay else {return}
-        guard _playerNumber != 0 else {return}
+//        guard case diceGameManager._dicesToPlay else {return}
+//        guard let _roundsToPlay else {return}
+//        guard _playerNumber != 0 else {return}
 //        guard  case _dicesToPlay ,  case _roundsToPlay, _playerNumber != 0 else {return}
-        playButton.isHidden = false
         
-        settingsButton.isHidden = true
+        if self.diceGameManager._dicesToPlay != nil, self.diceGameManager._roundsToPlay != nil, self.diceGameManager._roundsToPlay != nil{
+            
+            playButton.isHidden = false
+            settingsButton.isHidden = true
+        }
+        
+        
     }
     
     @IBAction func playBtn(_ sender: UIButton) {
